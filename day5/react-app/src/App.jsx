8643f0e7a7f21f2c5a3ec24cfa2656a64c7bc862
Prabhat-kumar-11 <prabhssgg3@gmail.com>
2025-06-12 09:43:06 +0530

    state

--- day5/react-app/src/App.jsx
+++ day5/react-app/src/App.jsx
@@ -1,15 +1,33 @@
-import React from 'react'
-
-
+import React, { useState } from "react";
+import ChildCom from "./components/ChildCom";
 
 // what is State in React JS
 // What is Props in React JS
 // Example: City, state, Country
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+  const [name, setName] = useState("prabhat"); //help in state management in functional component  [value , function]
+  const [city, setCity] = useState("delhi");
+  const [country, setCountry] = useState("india");
 
-export default App
+  let game = "pubg";
+  function handleChange() {
+    game = "csgo";
+    setName("batman");
+    setCity("gothamcity");
+    setCountry("america");
+  }
+
+  return (
+    <div>
+      <h1>Name:{name}</h1>
+      <h2>City:{city}</h2>
+      <h3>Country:{country}</h3>
+      <h4>game: {game}</h4>
+      <button onClick={handleChange}>Change name</button>
+      <ChildCom name={name}  city={city}/>
+    </div>
+  );
+};
+
+export default App;
---
+++ day5/react-app/src/components/ChildCom.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ChildCom = ({name,city}) => {
+  return (
+    <div>
+      <p>Name:{name}</p>
+      <p>City:{city}</p>
+    </div>
+  )
+}
+
+export default ChildCom
